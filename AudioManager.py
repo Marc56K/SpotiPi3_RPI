984dcf5aa8cd218555c6bd828d1bc4ec239904c3
Marc Roßbach <>
2020-05-03 21:48:50 +0200

    feat: headphone detection

--- AudioManager.py
+++ AudioManager.py
@@ -1,4 +1,5 @@
 from UpdateThread import UpdateThread
+import RPi.GPIO as gpio
 import alsaaudio as audio
 import queue
 import os
@@ -6,13 +7,39 @@ import logging
 import json
 import math
 
-class AudioManager:
+class AudioManager(UpdateThread):
 
+    _volume = 0
     _maxVolume = 100
     _minVolume = 0
+    _cardIdx = 0
 
     def __init__(self):
+        UpdateThread.__init__(self, "AUDIO-Thread")
+        self.setSleepDuration(0.5)
         self._soundsDir = os.path.dirname(__file__) + "/sounds/"
+        self._cardIdx = audio.cards().index('wm8960soundcard')
+        gpio.setmode(gpio.BCM)
+        gpio.setup(4, gpio.IN)
+
+    def update(self):
+        try:
+            if self.headphoneDetected():                
+                self.getSpeakerMixer().setvolume(0)
+            else:
+                self.getSpeakerMixer().setvolume(self._volume)
+            self.getHeadphoneMixer().setvolume(self._volume)
+        except Exception as e:
+            logging.error(str(e))
+
+    def headphoneDetected(self):
+        return not gpio.input(4)
+
+    def getSpeakerMixer(self):
+        return audio.Mixer('Speaker', cardindex = self._cardIdx)
+
+    def getHeadphoneMixer(self):
+        return audio.Mixer('Headphone', cardindex = self._cardIdx)
 
     def playSound(self, fileName):
         try:
@@ -21,15 +48,10 @@ class AudioManager:
             logging.error(str(e))
 
     def setAudioVolume(self, vol):
-        try:
-            for m in self.getMixer():
-                newVol = 0
-                if vol > 0:
-                    newVol = 19.87266007 * math.log(vol) + 7.893804667
-                newVol = max(self._minVolume, min(self._maxVolume, round(newVol)))
-                m.setvolume(int(newVol))
-        except Exception as e:
-            logging.error(str(e))
-
-    def getMixer(self):
-        return [ audio.Mixer('Headphone', cardindex = 0), audio.Mixer('Speaker', cardindex = 0) ]
+        with self.cv:        
+            newVol = 0
+            if vol > 0:
+                newVol = 19.87266007 * math.log(vol) + 7.893804667
+            newVol = max(self._minVolume, min(self._maxVolume, round(newVol)))
+            self._volume = newVol
+            self.cv.notify_all()
